0c5ef64f389dce6a59acad3cd1d32e8cd101781f
PartitionTheory <setmaths@gmail.com>
2026-09-01 21:46:24 +0100

    ABR v0.4-greenbuild: add synthetic math activation operator (synth.math.activate) and integrate into synth init

--- src/synth/abr_synth_init.c
+++ src/synth/abr_synth_init.c
@@ -7,6 +7,7 @@
 #include "abr_synth_math_mul_op.h"
 #include "abr_synth_math_pow_op.h"
 #include "abr_synth_math_basic_set.h"
+#include "abr_synth_math_activate_op.h"
 
 /*
  * Phoenix v0.4-greenbuild:
@@ -34,6 +35,7 @@ void abr_synth_init(abr_context_t* ctx)
 
     /* Initialize math families */
     abr_synth_math_basic_set_init();
+    abr_synth_math_activate_op_register(ctx);
 
     /* Register set-activation operator */
     abr_synth_activate_op_register(ctx);
