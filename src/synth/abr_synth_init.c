9ded3e73dc03946b66f2b7efd56ef22a59da1480
PartitionTheory <setmaths@gmail.com>
2026-09-01 20:14:45 +0100

    ABR v0.4-greenbuild: introduce synthetic operator set registry and migrate init to set-based registration

--- src/synth/abr_synth_init.c
+++ src/synth/abr_synth_init.c
@@ -1,6 +1,7 @@
 #include "abr_context.h"
 #include "abr_synth_template_op.h"
 #include "abr_synth_json_op.h"
+#include "abr_synth_set.h"
 
 /*
  * Phoenix v0.4-greenbuild:
@@ -9,14 +10,13 @@
  * This function is called during ABR system initialization.
  * It registers all synthetic operators.
  */
-
 void abr_synth_init(abr_context_t* ctx)
 {
-    /* Register Synthetic Operator #1 */
-    abr_synth_template_op_register(ctx);
+    /* Add operators to the core synthetic set */
+    abr_synth_set_add("synth.core", &synth_template_op);
+    abr_synth_set_add("synth.core", &synth_json_op);
 
-    /* Additional synthetic operators will be added in Step 36+ */
-    /* Synthetic Operator #2 */
-    abr_synth_json_op_register(ctx);
+    /* Register all operators in the core set */
+    abr_synth_set_register_all(ctx, "synth.core");
 }
 
