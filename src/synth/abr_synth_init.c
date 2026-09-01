e5243ffa9361468c802ca2b76e18d2415c8ecdc5
PartitionTheory <setmaths@gmail.com>
2026-09-01 21:06:20 +0100

    ABR v0.4-greenbuild: add synthetic set-activation operator and integrate into synth init

--- src/synth/abr_synth_init.c
+++ src/synth/abr_synth_init.c
@@ -2,6 +2,7 @@
 #include "abr_synth_template_op.h"
 #include "abr_synth_json_op.h"
 #include "abr_synth_set.h"
+#include "abr_synth_activate_op.h"
 
 /*
  * Phoenix v0.4-greenbuild:
@@ -12,11 +13,14 @@
  */
 void abr_synth_init(abr_context_t* ctx)
 {
-    /* Add operators to the core synthetic set */
     abr_synth_set_add("synth.core", &synth_template_op);
     abr_synth_set_add("synth.core", &synth_json_op);
 
-    /* Register all operators in the core set */
+    /* Register core operators */
     abr_synth_set_register_all(ctx, "synth.core");
+
+    /* Register the set-activation operator */
+    abr_synth_activate_op_register(ctx);
 }
+
 
