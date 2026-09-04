87670d1963ac2210b68a69299c2a552c00841f55
PartitionTheory <setmaths@gmail.com>
2026-09-04 13:15:28 +0100

    declare the builtin plugin tables

---
+++ core/loader/abr_math_v4_plugins.h
@@ -0,0 +1,11 @@
+#ifndef ABR_MATH_V4_PLUGINS_H
+#define ABR_MATH_V4_PLUGINS_H
+
+#include "abr_plugin.h"
+
+/* math_v4 plugins (v0.5 static table). */
+extern abr_plugin* abr_math_v4_plugins[];
+extern const size_t abr_math_v4_plugin_count;
+
+#endif /* ABR_MATH_V4_PLUGINS_H */
+
