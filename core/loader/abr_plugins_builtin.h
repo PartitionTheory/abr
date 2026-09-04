8bbd0ef5a4e93f5a3676b11d4a68d7a5133daa0a
PartitionTheory <setmaths@gmail.com>
2026-09-04 13:14:29 +0100

    header to declare plugin table

---
+++ core/loader/abr_plugins_builtin.h
@@ -0,0 +1,11 @@
+#ifndef ABR_PLUGINS_BUILTIN_H
+#define ABR_PLUGINS_BUILTIN_H
+
+#include "abr_plugin.h"
+
+/* Built-in plugins (v0.5 static table). */
+extern abr_plugin* abr_builtin_plugins[];
+extern const size_t abr_builtin_plugin_count;
+
+#endif /* ABR_PLUGINS_BUILTIN_H */
+
