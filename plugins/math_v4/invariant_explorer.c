64c2fa8236512071cd8685dc1ea2c34b76b3ac8f
PartitionTheory <setmaths@gmail.com>
2026-09-04 11:16:36 +0100

    added headers

--- plugins/math_v4/invariant_explorer.c
+++ plugins/math_v4/invariant_explorer.c
@@ -17,6 +17,9 @@
 #include "abr.h"
 #include "abr_plugin.h"
 #include "abr_math_v4_common.h"
+#include "abr_operators.h"
+#include "abr_operator_chain.h"
+#include "abr_plugin_registry.h"
 
 #include <stdlib.h>
 
